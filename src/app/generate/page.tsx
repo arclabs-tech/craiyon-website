7f63697c07e660a1b74185721308d96182ce1f19
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-01-24 11:48:15 +0530

    add image gen schema

--- src/app/generate/page.tsx
+++ src/app/generate/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+
+export default function GeneratePage() {
   return <h1>Hi</h1>;
 }
--- src/lib/schemas.ts
+++ src/lib/schemas.ts
@@ -5,3 +5,48 @@ export const teamLoginSchema = z.object({
   password: z.string().min(8).max(50),
 });
 export type TeamLoginSchema = z.infer<typeof teamLoginSchema>;
+
+export const stylePresets = [
+  "",
+  "3d-model",
+  "analog-film",
+  "anime",
+  "cinematic",
+  "comic-book",
+  "digital-art",
+  "enhance",
+  "fantasy-art",
+  "isometric",
+  "line-art",
+  "low-poly",
+  "neon-punk",
+  "origami",
+  "photographic",
+  "pixel-art",
+  "texture",
+  "craft-clay",
+] as const;
+
+export const imageOptsSchema = z.object({
+  model: z.string(),
+  prompt: z.string().min(0).max(2000),
+  negative_prompt: z.string().min(0).max(2000),
+  style_preset: z.enum(stylePresets),
+  steps: z.number().min(1).max(25),
+  cfg_scale: z.number().min(1).max(20),
+  seed: z.literal(1),
+  sampler: z.string(),
+  width: z.literal(1024),
+  height: z.literal(1024),
+});
+
+export type ImageOpts = z.infer<typeof imageOptsSchema>;
+
+export const imageEntrySchema = z.object({
+  team_name: z.string().min(3).max(50),
+  image_url: z.string().min(3).max(2000),
+  opts: imageOptsSchema,
+  created_at: z.date(),
+});
+
+export type ImageEntry = z.infer<typeof imageEntrySchema>;
