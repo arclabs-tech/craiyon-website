beabf0ff5a276e8e17ed49184aea38f5447a1a6a
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-01-24 16:37:23 +0530

    remove external image optimization

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.prodia.xyz',
-      },
-    ],
-  },
-};
+const nextConfig = {};
 
 export default nextConfig;
