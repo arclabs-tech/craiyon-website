3d6197c4b7b15978d898a5d3df8c4a8be7be3411
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-01-27 12:15:38 +0530

    add tabs in submissions

--- src/app/dashboard/submissions/page.tsx
+++ src/app/dashboard/submissions/page.tsx
@@ -1,7 +1,9 @@
 import { cookies } from "next/headers";
 import { getEntriesByTeamName } from "@/actions/imageEntries";
 import { DataTable } from "./data-table";
-import { columns } from "./columns";
+import { columns as imageColumns } from "./columns";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default async function Submissions() {
   const cookieStore = cookies();
@@ -11,12 +13,27 @@ export default async function Submissions() {
   }
   const team_name = team_name_cookie.value;
 
-  const entries = await getEntriesByTeamName(team_name);
+  const imageEntries = await getEntriesByTeamName(team_name);
   return (
     <div className="flex flex-col p-8 gap-4 items-center">
       <h1 className="text-4xl font-semibold">Your Submissions</h1>
-      <div className="rounded-2xl border-2 w-full lg:w-1/2">
-        <DataTable columns={columns} data={entries} />
+      <div className="w-full lg:w-1/2">
+        <Tabs defaultValue="images" className="w-full">
+          <TabsList className="w-full lg:h-14">
+            <TabsTrigger className="w-full lg:h-12 lg:text-lg" value="images">
+              Images
+            </TabsTrigger>
+            <TabsTrigger className="w-full lg:h-12 lg:text-lg" value="text">
+              Text
+            </TabsTrigger>
+          </TabsList>
+          <TabsContent value="images">
+            <DataTable columns={imageColumns} data={imageEntries} />
+          </TabsContent>
+          <TabsContent value="text">
+            <DataTable columns={imageColumns} data={imageEntries} />
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );
