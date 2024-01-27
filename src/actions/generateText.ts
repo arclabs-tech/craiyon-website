1ecf9870de0480f6a609b9b40aa6d3c3526a05d2
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-01-27 17:02:11 +0530

    add llama for text generation

--- src/actions/generateText.ts
+++ src/actions/generateText.ts
@@ -2,12 +2,21 @@
 
 import OpenAI from "openai";
 import { type TextOpts } from "@/lib/schemas";
+import axios from "axios";
 
 const openai = new OpenAI({
   apiKey: process.env.OPENAI_API_KEY,
 });
 
 export async function generateText(opts: TextOpts) {
+  if (opts.model == "gpt-3.5-turbo-1106") {
+    return await generateTextGPT3(opts);
+  } else if (opts.model == "llama-2-13b") {
+    return await generateTextLlama(opts);
+  }
+}
+
+async function generateTextGPT3(opts: TextOpts) {
   const completion = await openai.chat.completions.create({
     messages: [
       {
@@ -25,6 +34,28 @@ export async function generateText(opts: TextOpts) {
   });
   const generation = completion.choices[0].message.content;
   return generation;
+}
+
+async function generateTextLlama(opts: TextOpts) {
+  const prompt = opts.system_prompt + opts.user_prompt;
+
+  const { data, status } = await axios.post<{ generation: string }>(
+    "https://abh4g3zbqx33mutjmi3o52jzzq0ndtft.lambda-url.us-east-1.on.aws/",
+    {
+      prompt: prompt,
+      max_gen_len: opts.max_tokens,
+      temperature: opts.temperature[0],
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+  if (status !== 200)
+    throw new Error(`Error ${status}: Failed to get image embedding`);
+
+  return data.generation;
 }
 
 export async function getEmbedding(text: string) {
