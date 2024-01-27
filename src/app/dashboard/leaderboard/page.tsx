52d570647bba5f1edc1f6e482e9184b9058c8cb1
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-01-27 17:51:40 +0530

    Remove SSR in leaderboard

--- src/app/dashboard/leaderboard/page.tsx
+++ src/app/dashboard/leaderboard/page.tsx
@@ -1,12 +1,9 @@
 import { getLeaderboard } from "@/actions/leaderboard";
-import { LeaderboardEntry, columns } from "./columns";
+import { columns } from "./columns";
 import { DataTable } from "./data-table";
 
-export default async function LeaderBoard({
-  entries,
-}: {
-  entries: LeaderboardEntry[];
-}) {
+export default async function LeaderBoard() {
+  const entries = await getLeaderboard();
   return (
     <div className="flex flex-col p-8 gap-4 items-center">
       <h1 className="text-4xl font-semibold">Leaderboard</h1>
@@ -15,13 +12,4 @@ export default async function LeaderBoard({
       </div>
     </div>
   );
-}
-
-export async function getServerSideProps() {
-  const entries = await getLeaderboard();
-  return {
-    props: {
-      entries,
-    },
-  };
 }
