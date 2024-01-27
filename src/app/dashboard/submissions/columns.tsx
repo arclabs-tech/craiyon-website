5e42555b5b0d131c5119c8fe0bfc3eada8486561
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-01-27 16:11:18 +0530

    add slug for text id

--- src/app/dashboard/submissions/columns.tsx
+++ src/app/dashboard/submissions/columns.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { ImageEntry } from "@/lib/schemas";
+import { ImageEntry, TextEntry } from "@/lib/schemas";
 import { ColumnDef } from "@tanstack/react-table";
 import { MoreInfo } from "./more-info";
 
@@ -23,4 +23,26 @@ export const columns: ColumnDef<ImageEntry>[] = [
     id: "more-info",
     cell: ({ row }) => <MoreInfo entry={row.original} />,
   },
+];
+
+export const textColumns: ColumnDef<TextEntry>[] = [
+  {
+    accessorKey: "created_at",
+    header: () => <h1 className="w-18">Created At</h1>,
+    cell: ({ row }) => (
+      <p className="w-18">{row.original.created_at.toLocaleString()}</p>
+    ),
+  },
+  {
+    accessorKey: "text_id",
+    header: () => <h1 className="w-18">Image No.</h1>,
+  },
+  {
+    accessorKey: "score",
+    header: "Score",
+  },
+  // {
+  //   id: "more-info",
+  //   cell: ({ row }) => <MoreInfo entry={row.original} />,
+  // },
 ];
--- src/app/dashboard/text/page.tsx
+++ src/app/dashboard/text/page.tsx
@@ -1,139 +1,21 @@
-"use client";
-
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-
 import { Button } from "@/components/ui/button";
-import {
-  Model,
-  SystemPrompt,
-  UserPrompt,
-  Temperature,
-  MaxTokens,
-  formContext,
-} from "@/components/generateText";
-import { Form } from "@/components/ui/form";
-import { textOptsSchema, type TextOpts, TextEntry } from "@/lib/schemas";
-import { Skeleton } from "@/components/ui/skeleton";
+import Link from "next/link";
 
-import { generateText, getEmbedding } from "@/actions/generateText";
-import { useState } from "react";
-import { getTextSrcEmbeddings } from "@/lib/embeddings";
-import { cosineSimilarity } from "@/lib/similarity";
-import { getCookie } from "cookies-next";
-import { addTextEntry, getNumOfEntriesByTextId } from "@/actions/textEntries";
-
-enum State {
-  Generate,
-  Checking,
-  Generating,
-  GeneratingEmbedding,
-  Calculating,
-  Submitting,
-  Next,
+export default function Index() {
+  return (
+    <div className="flex flex-col gap-4 px-6 items-center">
+      <h1 className="text-6xl font-semibold text-center">Choose an image</h1>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-4">
+        <Text id="1" />
+      </div>
+    </div>
+  );
 }
 
-export default function TextGen() {
-  const textId = 1;
-  const [state, setState] = useState<State>(State.Generate);
-  const [generatedText, setGeneratedText] = useState<string | null>(null);
-  const [similarity, setSimilarity] = useState<number>(0);
-  const [remaining, setRemaining] = useState<number>(25);
-  const team_name = getCookie("team_name") as string;
-  const form = useForm<TextOpts>({
-    resolver: zodResolver(textOptsSchema),
-    defaultValues: {
-      model: "gpt-3.5-turbo-1106",
-      system_prompt: "You are Maria",
-      user_prompt: "Hello, I am Robert",
-      temperature: [0.5],
-      max_tokens: 50,
-    },
-  });
-
-  async function onSubmit(data: TextOpts) {
-    if (state == State.Next) {
-      setState(State.Generate);
-      return;
-    }
-    setState(State.Checking);
-    const numOfRemainingTexts =
-      25 - (await getNumOfEntriesByTextId(textId, team_name));
-    if (numOfRemainingTexts <= 0) {
-      alert("You have reached the limit of 25 submissions");
-      return;
-    }
-    setRemaining(numOfRemainingTexts);
-    setState(State.Generating);
-    const response = await generateText(data);
-    if (response == null) {
-      setState(State.Generate);
-      return;
-    }
-    setGeneratedText(response);
-    setState(State.GeneratingEmbedding);
-    const embedding = await getEmbedding(response);
-    const srcEmbeddings = await getTextSrcEmbeddings();
-    setState(State.Calculating);
-    const similarity = cosineSimilarity(embedding, srcEmbeddings);
-    setSimilarity(similarity);
-    setState(State.Submitting);
-    const textEntry: TextEntry = {
-      text_id: textId,
-      created_at: new Date(),
-      generation: response,
-      score: similarity,
-      team_name: team_name,
-      ...data,
-      temperature: data.temperature[0],
-    };
-    await addTextEntry(textEntry);
-    setState(State.Next);
-  }
-
-  if (!Array.from({ length: 10 }, (_, i) => i + 1).includes(textId)) {
-    return "Text not found";
-  }
-
+function Text({ id }: { id: string }) {
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3">
-      <Form {...form}>
-        <form
-          onSubmit={form.handleSubmit((data) => onSubmit(data))}
-          className="w-full px-4"
-        >
-          <formContext.Provider value={form}>
-            <div className="flex flex-col gap-4">
-              <Model />
-              <SystemPrompt />
-              <UserPrompt />
-              <Temperature />
-              <MaxTokens />
-              <div className="flex flex-row items-center gap-4">
-                <Button
-                  type="submit"
-                  className="w-48"
-                  disabled={!(state == State.Generate || state == State.Next)}
-                >
-                  {State[state]}
-                </Button>
-                {state >= State.Next && (
-                  <div>
-                    <p>Score = {similarity.toFixed(10)}</p>
-                    <p>{remaining} submissions remaining</p>
-                  </div>
-                )}
-              </div>
-            </div>
-          </formContext.Provider>
-        </form>
-      </Form>
-      <p>Required text</p>
-      {state >= State.Generating && state < State.Next ? (
-        <Skeleton className="w-[90%] h-full p-4" />
-      ) : (
-        <p>{generatedText ? generatedText : "Your generated text here"}</p>
-      )}
-    </div>
+    <Link href={`/dashboard/text/${id}`}>
+      <Button variant={"outline"}>Text {id}</Button>
+    </Link>
   );
 }
