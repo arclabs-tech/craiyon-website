701e8c3406d191237974068a82fc2f14fcf019a2
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-01-28 09:58:04 +0530

    highlight current team's entry

--- src/app/dashboard/leaderboard/columns.tsx
+++ src/app/dashboard/leaderboard/columns.tsx
@@ -7,13 +7,33 @@ export type LeaderboardEntry = {
   total_score: number;
 };
 
-export const columns: ColumnDef<LeaderboardEntry>[] = [
-  {
-    accessorKey: "team_name",
-    header: "Team Name",
-  },
-  {
-    accessorKey: "total_score",
-    header: "Score",
-  },
-];
+export function getColumns(team_name: string) {
+  const columns: ColumnDef<LeaderboardEntry>[] = [
+    {
+      accessorKey: "team_name",
+      header: "Team Name",
+      cell: ({ row }) => {
+        const entry = row.original;
+        if (entry.team_name == team_name) {
+          return <b>{entry.team_name}</b>;
+        } else {
+          return entry.team_name;
+        }
+      },
+    },
+    {
+      accessorKey: "total_score",
+      header: "Score",
+      cell: ({ row }) => {
+        const entry = row.original;
+        if (entry.team_name == team_name) {
+          return <b>{entry.total_score}</b>;
+        } else {
+          return entry.total_score;
+        }
+      },
+    },
+  ];
+
+  return columns;
+}
--- src/app/dashboard/leaderboard/leaderboard.tsx
+++ src/app/dashboard/leaderboard/leaderboard.tsx
@@ -1,22 +1,16 @@
-"use server";
+"use client";
 
-import { getLeaderboard } from "@/actions/leaderboard";
-import { LeaderboardEntry, columns } from "./columns";
+import { LeaderboardEntry, getColumns } from "./columns";
 import { DataTable } from "./data-table";
 
-export default async function LeaderBoard() {
-  let entries: LeaderboardEntry[] = [];
-  try {
-    entries = await getLeaderboard();
-  } catch (e) {
-    console.error(e);
-  }
-  return (
-    <div className="flex flex-col p-8 gap-4 items-center">
-      <h1 className="text-4xl font-semibold">Leaderboard</h1>
-      <div className="w-full lg:w-1/2">
-        <DataTable columns={columns} data={entries} />
-      </div>
-    </div>
-  );
+export default function LeaderBoard({
+  team_name,
+  entries,
+}: {
+  team_name: string;
+  entries: LeaderboardEntry[];
+}) {
+  "use client";
+  const columns = getColumns(team_name);
+  return <DataTable columns={columns} data={entries} />;
 }
