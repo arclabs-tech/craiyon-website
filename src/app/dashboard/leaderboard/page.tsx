e56479a27ec54bf3baf5fa99975a41924deacdfb
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-01-28 09:16:20 +0530

    Move to another component

--- src/app/dashboard/leaderboard/page.tsx
+++ src/app/dashboard/leaderboard/page.tsx
@@ -1,15 +1,3 @@
-import { getLeaderboard } from "@/actions/leaderboard";
-import { columns } from "./columns";
-import { DataTable } from "./data-table";
+import LeaderBoard from "./leaderboard";
 
-export default async function LeaderBoard() {
-  const entries = await getLeaderboard();
-  return (
-    <div className="flex flex-col p-8 gap-4 items-center">
-      <h1 className="text-4xl font-semibold">Leaderboard</h1>
-      <div className="w-full lg:w-1/2">
-        <DataTable columns={columns} data={entries} />
-      </div>
-    </div>
-  );
-}
+export default LeaderBoard;
