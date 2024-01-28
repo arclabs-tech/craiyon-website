7fbbe608ddda9c75c21dfca4ea94713219599275
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-01-28 09:21:52 +0530

    Add try catch to leaderboard fetch

--- src/app/dashboard/leaderboard/leaderboard.tsx
+++ src/app/dashboard/leaderboard/leaderboard.tsx
@@ -1,11 +1,16 @@
 "use server";
 
 import { getLeaderboard } from "@/actions/leaderboard";
-import { columns } from "./columns";
+import { LeaderboardEntry, columns } from "./columns";
 import { DataTable } from "./data-table";
 
 export default async function LeaderBoard() {
-  const entries = await getLeaderboard();
+  let entries: LeaderboardEntry[] = [];
+  try {
+    entries = await getLeaderboard();
+  } catch (e) {
+    console.error(e);
+  }
   return (
     <div className="flex flex-col p-8 gap-4 items-center">
       <h1 className="text-4xl font-semibold">Leaderboard</h1>
