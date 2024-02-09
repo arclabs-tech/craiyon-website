3106a61dc1286baa56a0e36a22c4f024ec354a51
Sujay R <68642484+sujay1844@users.noreply.github.com>
2024-02-09 18:51:58 +0530

    add team1 cuz I can't delete it for some reason

--- src/lib/login.ts
+++ src/lib/login.ts
@@ -22,19 +22,26 @@ const teams = [
   ["Bhuvan S", "Bhuvan S"],
   ["CVP", "CVP"],
   ["Abhishek Bhat", "Abhishek Bhat"],
+  ["team1", "team1"],
 ];
 
 export const hashedTeams = new Map<
   string,
   { team_name: string; password: string }
->();
-for (const [team_name, password] of teams) {
-  hashedTeams.set(hash(team_name), {
-    team_name,
-    password,
-  });
-}
-
+>(
+  teams.map(([team_name, password]) => [
+    hash(team_name),
+    {
+      team_name,
+      password,
+    },
+  ])
+);
 export function getTeamName(hashed: string) {
-  return hashedTeams.get(hashed)!.team_name;
+  const team = hashedTeams.get(hashed);
+  if (team) {
+    return team.team_name;
+  } else {
+    return "";
+  }
 }
